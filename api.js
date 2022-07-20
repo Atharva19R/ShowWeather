eadc9cbab113c49946f14e2e699feb7d31e6dde6
Atharva R <dangerrath@gmail.com>
2022-07-20 15:54:10 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "d9e4c777demsh7588f9da7430df2p176978jsn808782afae62",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "bb0e7a9ac68d04297004e00f3a65ad85"; // enter your key from openweather API
